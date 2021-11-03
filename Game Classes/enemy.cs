e79a8bb12d1b5f483ccf3936073199b7361b7bd9
Kabuum <thomsenribermartin@gmail.com>
2021-11-03 13:55:27 +0100

    enemy things

--- Game Classes/enemy.cs
+++ Game Classes/enemy.cs
@@ -30,15 +30,15 @@ namespace rpg_game.Game_Classes
 
             new Enemy("Pineapple on pizza hater","I can see it in you. You enjoy pineapple on pizza!!!.You uncultured beast I will cut you down"
                 ,1,2,
-                "Nooo this can't happen! I can't die! Who will protect pizzas from pineapples."),
+                "Nooo this can't happen! I can't die! Who will protect pizzas from pineapples now."),
 
             new Enemy("Socks and sandals wearer","What is this?!?! I see you're wearing socks but NO SANDALS. YOU FIEND. That is a crime against the church of SOCKS AND SANDALS."
                 ,3,2,
                 "HOW!? how could this happen, Please god send me to a better place in the after after life. A place where sock and sandal may live together in harmony..."),
 
-            new Enemy("DEMON (except he has no arms, or legs for that matter)","BEWARE HUMAN. I may have no arms or legs. BUT I'M DEFINTELY A DANGEROUS DEMON AND I WILL TORMENT YOU."
+            new Enemy("DEMON (except he has no arms, or legs)","BEWARE HUMAN. I may have no arms or legs. BUT I'M DEFINTELY A DANGEROUS DEMON AND I WILL TORMENT YOU."
                 ,9,0,
-                "Not fair alright "),
+                "Not fair"),
 
             new Enemy("name","intro"
                 ,1,1,
