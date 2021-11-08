e12888f6fb149410693ba134793bb40ea7f150e8
Kabuum <thomsenribermartin@gmail.com>
2021-11-08 14:58:22 +0100

    new enemies and small red demon tower WIP

--- Game Classes/enemy.cs
+++ Game Classes/enemy.cs
@@ -32,19 +32,22 @@ namespace rpg_game.Game_Classes
 
             new Enemy("Pineapple on pizza hater","I can see it in you. You enjoy pineapple on pizza!!! You uncultured beast I will cut you down"
                 ,1,2,
-                "Nooo this can't happen! I can't die! Who will protect pizzas from pineapples now."),
+                "Nooo this can't happen! I can't die! I had just figured out the secret password to the outpost IT WAS JUST PINEAPPLE BACKWARDS, Who will protect pizzas from pineapples now."),
 
             new Enemy("Socks and sandals wearer","What is this?!?! I see you're wearing socks but NO SANDALS. YOU FIEND. That is a crime against the church of SOCKS AND SANDALS."
                 ,3,2,
                 "HOW!? how could this happen, Please god send me to a better place in the after after life. A place where sock and sandal may live together in harmony..."),
 
-            new Enemy("DEMON (except he has no arms, or legs for that matter)","BEWARE HUMAN. I may have no arms or legs. BUT I'M DEFINTELY A DANGEROUS DEMON AND I WILL TORMENT YOU."
+            new Enemy("Demon (except he has no arms, or legs for that matter)","BEWARE HUMAN. I may have no arms or legs. BUT I'M DEFINTELY DANGEROUS AND I WILL TORMENT YOU."
                 ,9,0,
-                "Not fair"),
+                "It turns out you need arms and legs to deal any damage to your opponent, Curse that green guy with the helmet he sliced them off"),
 
             new Enemy("Small red demon","They don't make any small size tridents but they had a spare normal size one. It may be too big for me but I'll use it if I must"
                 ,3,4,
                 "You've defeated me human, I wonder where I'll go now that I'm dead, Hells Hell? I guess I'll see"),
+
+            new Enemy("BIG ASS Demon", "I'm terribly sorry but you're not on the list to get out of hell. Did you send in your appeal on our website? Ahhh screw it. I'll just beat the shit out of you"
+                ,20, 5,"HOW! It can't be. A puny human like you defeating me, I was the unstoppable guy protecting the door. I guess I'll have to allow you through the door. Have fun going back to that hellhole called earth.")
         };
 
     }
--- Game Scenes/The tower of the small red demons.cs
+++ Game Scenes/The tower of the small red demons.cs
@@ -9,11 +9,12 @@ namespace rpg_game.Game_Scenes
     class SmallRedDemonTower : Scene
     {
         public override void Start(ref Player plr)
-        {        
-            Program.print($"{plr.name} You've died");
-            Program.print("", delay: 1000);
-            Program.print("It's up to you", delay: 100, withNLine: false);
-
+        {
+            Program.print("There's a tower before you.", delay: 200, withNLine: false); Program.print("the tower is about 4 times as tall as you, but only has like a 2 meter diameter."delay: 200, withNLine: false); Program.print("and the door is really small");
+            Program.print("outside the tower there's a little sign that says");
+            Program.print("'Welcome to the red demon tower", delay: 200);
+            Program.print("MAXIMUM HEIGHT 3'0 ft'", delay: 200);
+            Program.print("Looks like America isn't the only place that still doesn't use the metric system.", delay: 200, withNLine: false); Program.print("Or maybe just a lot of Americans go to hell hmm...", delay: 200, withNLine: false); Program.print("I'll let you interpret that one for yourself");
         }
     }
 }
