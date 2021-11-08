3b3dfd0f3f1db54f375a43b01f2b1098ac7744c9
Kabuum <thomsenribermartin@gmail.com>
2021-11-08 11:21:01 +0100

    Locations WIP

--- Game Classes/Location.cs
+++ Game Classes/Location.cs
@@ -9,17 +9,22 @@ namespace rpg_game.Game_Classes
     {
         public string name;
         public Vector pos;
+        public Game_Scenes.Scene onEnter = null;
 
-        public Location(string locationName, Vector locationPos)
+        public Location(string locationName, Vector locationPos, Game_Scenes.Scene scene)
         {
             name = locationName;
             pos = locationPos;
+            onEnter = scene;
         }
 
         public static Location[] locations =
         {
-            new Location("The town of beginnigs", new Vector(0, 0)),
-            new Location("The town of continuation", new Vector(60, 20))
+            new Location("The town of beginnings", new Vector(0, 0), new Game_Scenes.Beginning()),
+            new Location("The outpost of pineapple on pizza lovers ", new Vector(60, 20), null),
+            new Location("The church of Socks and Sandals ", new Vector(-40, 30), null),
+            new Location("The tower of the small red demons ", new Vector(-70, 30), null),
+            new Location("The gates to hell", new Vector(-40, 30), null),
         };
     }
 }
