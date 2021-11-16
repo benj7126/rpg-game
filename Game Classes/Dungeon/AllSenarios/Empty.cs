43e070437c8a98fc87fc7131a5beb30610932997
catNull <benvb2004@gmail.com>
2021-11-16 21:23:43 +0100

    starting some dungeon thingy... last day wow...

---
+++ Game Classes/Dungeon/AllSenarios/Empty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rpg_game.Game_Classes.Dungeon.AllSenarios
+{
+    class Empty : senario
+    {
+        public override void onEnter()
+        {
+
+        }
+
+        public override void onLook()
+        {
+
+        }
+    }
+}
---
+++ Game Classes/Dungeon/Senario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rpg_game.Game_Classes.Dungeon
+{
+    class senario
+    {
+        public virtual void onEnter()
+        {
+
+        }
+
+        public virtual void onLook()
+        {
+
+        }
+
+        public static List<senario> senarioList = new List<senario>()
+        {
+
+        };
+    }
+}
