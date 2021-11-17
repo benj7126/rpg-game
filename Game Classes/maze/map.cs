fd1b723b6112a979cae879dee916801cdf015d06
Lasse Lykke Jensen <lasselj@yandex.com>
2021-11-17 08:32:29 +0100

    Comments, comments, comments.

--- Game Classes/maze/map.cs
+++ Game Classes/maze/map.cs
@@ -8,15 +8,20 @@ using System.Linq;
 namespace rpg_game.Game_Classes.maze
 {
     class Map {
+        // Save width and height.
         int width, height;
+        // Don't allow changing.
         public int Width{get => width;}
         public int Height{get => height;}
+        // Map is śaved as a list of ints. Different numbers have different
+        // functions/colors.
         public List<int> map = new List<int>();
 
         public Map(int sizeX, int sizeY) {
             width = sizeX;
             height = sizeY;
 
+            // Initialize map to empty list of correct size.
             map = new int[width * height].ToList();
         }
 
@@ -27,14 +32,18 @@ namespace rpg_game.Game_Classes.maze
             this.map = map.ToList();
         }
 
+        // Gets specific cell.
         public int GetCell(int x, int y) {
             return map[width-x + y * width];
         }
 
+        // Original SetCell function. Different X and Y from GetCell, since
+        // GetCell was changed to flip horizontally.
         public void SetCell(int x, int y, int cell) {
             map[x + y * width] = cell;
         }
 
+        // SetCell relative to GetCell.
         public void SetCellRel(int x, int y, int cell) {
             map[width-x + y * width] = cell;
         }
